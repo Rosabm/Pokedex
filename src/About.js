0aeb13b3e5513cb6e53ac4287ffb51792ae78716
Rosabm <rosabrialesm@gmail.com>
2023-03-31 17:11:15 +0200

    Adding video in about page
    Also some formating

--- src/About.js
+++ src/About.js
@@ -1,4 +1,15 @@
 export default function About() {
-    return <><h1>About</h1></>
+  return (
+    <>
+      <iframe
+        width="703"
+        height="395"
+        src="https://www.youtube.com/embed/yUT39VH2S6o"
+        title="All Pokémon Opening Theme Songs (with season 18)"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </>
+  );
 }
-
